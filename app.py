0f563a3e794126693394f5720bdd4db7ecff18a8
Sebastiano Moro <sebastianomoro97@gmail.com>
2023-11-29 22:06:51 +0100

    created the setup

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
-st.title('Hallo World')
-st.text('Questo è il primo testo')
+def main():
+    st.title('Hallo World')
+    st.text('Questo è il primo testo')
+
+if __name__ == '__main__':
+    main()
